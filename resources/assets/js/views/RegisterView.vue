f0300a6caabd1c1832287320d6bae93f08d955cf
Maksim Potashev <storo1017@gmail.com>
2017-10-18 16:00:23 +0000

    Create register form

--- resources/assets/js/views/RegisterView.vue
+++ resources/assets/js/views/RegisterView.vue
@@ -1,3 +1,26 @@
 <template>
-
+    <div class="container">
+        <form method="POST">
+            <div class="col s12">
+                <h2 class="center-align">Регистрация</h2>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="email" type="email" class="validate">
+                        <label for="email">Email</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="password" type="password" class="validate">
+                    <label for="password">Password</label>
+                </div>
+            </div>
+            <div class="row center-align">
+                <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+                    <i class="material-icons right">send</i>
+                </button>
+            </div>
+        </form>
+    </div>
 </template>
