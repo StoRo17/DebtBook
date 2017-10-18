37420adc4f09633006d84fd649c376b96a039a4e
Maksim Potashev <storo1017@gmail.com>
2017-10-18 14:47:27 +0000

    Add login and register views

--- resources/assets/js/startPageRoutes.js
+++ resources/assets/js/startPageRoutes.js
@@ -1,11 +1,24 @@
 import VueRouter from 'vue-router';
 import StartView from './views/StartView.vue';
+import RegisterView from './views/RegisterView.vue';
+import LoginView from './views/LoginView.vue';
+
 
 let routes = [
     {
         path: '/',
         name: 'startView',
         component: StartView
+    },
+    {
+        path: '/auth/register',
+        name: 'register',
+        component: RegisterView
+    },
+    {
+        path: '/auth/login',
+        name: 'auth',
+        component: LoginView
     }
 ];
 
