2089422cb9c6dcbdce7effbe6b7d16f98d8a6810
Maksim Potashev <storo1017@gmail.com>
2017-10-19 09:22:01 +0000

    Add simple v-model to RegisterForm

--- resources/assets/js/components/RegisterForm.vue
+++ resources/assets/js/components/RegisterForm.vue
@@ -5,22 +5,33 @@
                 <h2 class="center-align">{{ 'auth.sign_up' | trans }}</h2>
                 <div class="row">
                     <div class="input-field col s12">
-                        <input id="email" type="email" class="validate">
+                        <input id="email" type="email" class="validate" v-model="email">
                         <label for="email">Email</label>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s12">
-                    <input id="password" type="password" class="validate">
+                    <input id="password" type="password" class="validate" v-model="password">
                     <label for="password">{{ 'auth.password' | trans }}</label>
                 </div>
             </div>
             <div class="row center-align">
-                <button class="btn waves-effect waves-light" type="submit" name="action">{{ 'auth.submit' | trans }}
+                <button class="btn waves-effect waves-light" name="action">{{ 'auth.submit' | trans }}
                     <i class="material-icons right">send</i>
                 </button>
             </div>
         </form>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                email: '',
+                password: ''
+            }
+        }
+    }
+</script>
