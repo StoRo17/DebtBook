e8da96f7a86267d5678b5548e7058b8a2c4ad285
Maksim Potashev <storo1017@gmail.com>
2017-10-27 09:30:00 +0000

    Create methods

--- resources/assets/js/Errors.js
+++ resources/assets/js/Errors.js
@@ -4,5 +4,29 @@ export default class Errors {
         this.errors = {}
     }
 
-    
+    get(key) {
+        if (this.errors[key]) {
+            return this.errors[key][0];
+        }
+    }
+
+    set(errors) {
+        this.errors = errors;
+    }
+
+    has(key) {
+        return this.errors.hasOwnProperty(key);
+    }
+
+    any() {
+        return Object.keys(this.errors).length > 0;
+    }
+
+    clear(field) {
+        if (field) {
+            delete this.errors[field];
+        } else {
+            this.errors = {};
+        }
+    }
 }
