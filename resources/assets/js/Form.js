8bbc05dc769a690f5af5147cea14e50bac89b4b1
Maksim Potashev <storo1017@gmail.com>
2017-10-27 09:18:22 +0000

    Add data and submit methods

--- resources/assets/js/Form.js
+++ resources/assets/js/Form.js
@@ -1,12 +1,34 @@
+import axios from 'axios';
+
 export default class Form {
 
     constructor (data) {
-        this.data = data;
+        this.originalData = data;
 
         for (let field in data) {
             this[field] = data[field];
         }
     }
 
-    
+    data() {
+        let data = {};
+
+        for (let property in this.originalData) {
+            data[property] = this[property];
+        }
+
+        return data;
+    }
+
+    submit(requestType, url) {
+        return new Promise((resolve, reject) => {
+            axios[requestType](url, this.data())
+                .then(response => {
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    reject(error.response.data);
+                });
+        });
+    }
 }
