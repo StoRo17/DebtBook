974b426d0ba47ce9490403c78b58fd9de3cf4b82
Maksim Potashev <storo1017@gmail.com>
2017-11-05 14:41:58 +0000

    Create InputField component

---
+++ resources/assets/js/components/forms/InputField.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="input-field">
+        <input :id="name"
+               ref="input"
+               :type="type"
+               :name="name"
+               :class="inputClasses"
+               :value="value"
+               @input="updateText($event.target.value)">
+        <label :for="name" :data-error="error"><slot></slot></label>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['type', 'name', 'text', 'error', 'inputClasses'],
+
+        data() {
+            return {
+                value: this.text
+            }
+        },
+
+        methods: {
+            updateText(value) {
+                this.value = value;
+                this.$refs.input.value = value;
+                this.$emit('input', value);
+            }
+        }
+    }
+</script>
