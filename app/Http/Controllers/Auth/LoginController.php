e3ff8679a038963238c6abf742df7cc636e242c4
Maksim Potashev <storo1017@gmail.com>
2017-11-06 14:43:58 +0000

    Add verify validation logic to user email

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -38,6 +40,20 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
+    protected function attemptLogin(Request $request)
+    {
+        if ($this->verified($request->email)) {
+            return $this->guard()->attempt(
+                $this->credentials($request), $request->filled('remember')
+            );
+        }
+
+        throw ValidationException::withMessages([
+            'email' => ['Email has not been verified']
+        ])->status(422);
+    }
+
+
     protected function sendLoginResponse(Request $request)
     {
         $request->session()->regenerate();
@@ -47,5 +63,10 @@ class LoginController extends Controller
         return response()->json([
             'message' => 'User logged in'
         ]);
+    }
+
+    protected function verified($email)
+    {
+        return User::where('email', $email)->first()->verified;
     }
 }
