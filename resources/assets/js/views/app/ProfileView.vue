36b738a614c91eb97c9b869a094f65b6da4e4ae3
Maksim Potashev <storo1017@gmail.com>
2017-11-08 13:09:26 +0000

    Create first version of Profile view

---
+++ resources/assets/js/views/app/ProfileView.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col s12 center-align hide-on-large-only">
+                <a href="/" class="waves-light btn" style="margin-top: 10px;">Назад</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12 m6 l3">
+                <div class="center-align">
+                    <img class="circle responsive-img" src="storage/avatars/no_image.jpg">
+                </div>
+            </div>
+            <div class="col s12 m6 l9">
+                <ul class="collection with-header">
+                    <li class="collection-header"><h4>Профиль</h4></li>
+                    <li class="collection-item">Email: storo1017@gmail.com</li>
+                    <li class="collection-item">Имя: Максим</li>
+                    <li class="collection-item">Фамилия: Поташев</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12 l3 hide-on-med-and-down">
+                <div class="row">
+                    <div class="center-align">
+                        <a href="/" class="waves-light btn">Назад</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 l9">
+                <div class="row">
+                    <div class="right-align hide-on-med-and-down">
+                        <a href="" class="waves-light btn">Изменить данные</a>
+                        <a href="" class="waves-light btn">Сменить пароль</a>
+                        <a href="" class="waves-light btn">Выход</a>
+                    </div>
+                </div>
+                <div class="col s12 hide-on-large-only center-align">
+                    <div class="row">
+                        <a href="" class="waves-light btn">Изменить данные</a>
+                    </div>
+                    <div class="row">
+                        <a href="" class="waves-light btn">Сменить пароль</a>
+                    </div>
+                    <div class="row">
+                    </div>
+                    <div class="row">
+                        <a href="/" class="waves-light btn">Выход</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
