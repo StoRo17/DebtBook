9c040a4a8a0a5f01d777563cc1e8e6620641439f
Maksim Potashev <storo1017@gmail.com>
2017-11-11 10:49:55 +0000

    Add jquery minified file

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -18,7 +18,7 @@
     @else
         @include('startPage')
     @endif
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/materialize.min.js') }}"></script>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
