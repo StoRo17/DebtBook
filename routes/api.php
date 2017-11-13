6ed5b62ff27cca23ffb8504e45779e4481eb5b00
Maksim Potashev <storo1017@gmail.com>
2017-11-13 08:04:04 +0000

    Create route

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,6 @@ Route::get('/verify-email/{token}', 'Auth\RegisterController@verify')->name('ver
 Route::post('register', 'Auth\RegisterController@register')->name('register');
 Route::post('login', 'Auth\LoginController@login')->name('login');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('/users/{id}', 'UserController@show')->name('getUser');
 });
