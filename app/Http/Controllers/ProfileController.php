88b7afd5f692f76cbe299bf2141836d3899b8e4d
Maksim Potashev <storo1017@gmail.com>
2017-11-16 10:20:35 +0000

    Delete the show method, Profile resource and the route

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,14 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\Profile as ProfileResource;
-use App\Profile;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    public function show(Request $request, $id)
-    {
-        return new ProfileResource(Profile::find($id));
-    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,4 @@ Route::post('login', 'Auth\LoginController@login')->name('login');
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('/users/{id}', 'UserController@show')->name('getUser');
-    Route::get('/users/{id}/profile', 'ProfileController@show')->name('getProfile');
 });
