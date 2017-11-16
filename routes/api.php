7a2ac6e6513f396f16a121fecf146ab5c08a222f
Maksim Potashev <storo1017@gmail.com>
2017-11-16 11:12:55 +0000

    Add route and test for update method

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,5 @@ Route::post('login', 'Auth\LoginController@login')->name('login');
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('/users/{id}', 'UserController@show')->name('getUser');
+    Route::put('/users/{id}/profile', 'ProfileController@update')->name('updateProfile');
 });
---
+++ tests/Feature/ProfileControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Profile;
+use App\User;
+use Laravel\Passport\Passport;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProfileControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testFirstAndLastNamesUpdate()
+    {
+        $user = factory(User::class, 1)
+            ->states('verified')
+            ->create()
+            ->each(function ($u) {
+                $u->profile()->save(factory(Profile::class)->make());
+            })
+            ->first();
+
+        Passport::actingAs($user, ['*']);
+
+        $firstName = 'John';
+        $lastName = 'Doe';
+        $response = $this->putJson(route('updateProfile', $user->id), [
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+        ]);
+
+        $this->assertDatabaseHas('profiles', [
+            'first_name' => $firstName,
+            'last_name' => $lastName,
+        ]);
+        $response->assertJson([
+            'success' => true,
+            'message' => 'Profile updated.'
+        ]);
+
+        $responseArray = $response->json();
+        $this->assertEquals($firstName, $responseArray['data']['profile']['first_name']);
+        $this->assertEquals($lastName, $responseArray['data']['profile']['last_name']);
+    }
+}
