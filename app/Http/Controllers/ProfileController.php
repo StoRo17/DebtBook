168cae0ec4cc76bb23e70a64a803bb7822860ac7
Maksim Potashev <storo1017@gmail.com>
2017-11-16 10:43:43 +0000

    Add the update users profile method and Request for it

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ProfileRequest;
+use App\Http\Resources\User as UserResource;
 
 class ProfileController extends Controller
 {
+    public function update(ProfileRequest $request, $id)
+    {
+        $user = $request->user();
+        $user->profile()->update($request->all());
 
+        return (new UserResource($user))
+            ->additional([
+                'success' => true,
+                'message' => 'Profile updated.'
+            ]);
+    }
 }
