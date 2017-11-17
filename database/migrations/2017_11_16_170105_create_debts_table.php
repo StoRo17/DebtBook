e5d952c3b95c1af27935400da862fe36127fd1ff
Maksim Potashev <storo1017@gmail.com>
2017-11-17 12:27:27 +0000

    Create Debt model and migration for it

---
+++ database/migrations/2017_11_16_170105_create_debts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDebtsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('debts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->decimal('amount', 10);
+            $table->enum('currency', ['dollar', 'euro', 'ruble']);
+            $table->string('name');
+            $table->enum('type', ['give', 'take']);
+            $table->string('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('debts');
+    }
+}
