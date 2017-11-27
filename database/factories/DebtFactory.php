47941a4b4bc73e55748a89b2b14d35163e0ec897
Maksim Potashev <storo1017@gmail.com>
2017-11-27 09:47:37 +0000

    Update Debt factory

--- database/factories/DebtFactory.php
+++ database/factories/DebtFactory.php
@@ -4,10 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Debt::class, function (Faker $faker) {
     return [
-        'amount' => $faker->randomFloat(2, 0, 100000),
+        'total_amount' => $faker->randomFloat(2, 0, 100000),
         'currency' => 'ruble',
-        'name' => $faker->name,
-        'type' => $faker->randomElement(['give', 'take']),
-        'comment' => $faker->sentence
+        'name' => $faker->name
     ];
 });
