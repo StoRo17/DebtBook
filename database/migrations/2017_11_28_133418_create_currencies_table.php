489388a93ad512e678d94a8f00013b1289d6a629
Maksim Potashev <storo1017@gmail.com>
2017-11-28 13:47:29 +0000

    Create Currency model and migration for it

---
+++ database/migrations/2017_11_28_133418_create_currencies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('currency', 3)->unique();
+        });
+
+        DB::table('currencies')->insert([
+            ['currency' => 'RUB'],
+            ['currency' => 'USD'],
+            ['currency' => 'EUR'],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
