814227b304232f4c26166ade4c51a6710956c366
Maksim Potashev <storo1017@gmail.com>
2017-12-06 11:22:44 +0000

    Add show method

--- app/Http/Controllers/DebtsHistoryController.php
+++ app/Http/Controllers/DebtsHistoryController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\DebtsHistory;
 use Illuminate\Http\Request;
 
 class DebtsHistoryController extends Controller
 {
-    //
+    private $debtsHistory;
+
+    public function __construct(DebtsHistory $debtsHistory)
+    {
+        $this->debtsHistory = $debtsHistory;
+    }
+
+    public function show($debtId)
+    {
+        $debtHistory = $this->debtsHistory->where('debt_id', $debtId);
+
+        return $debtHistory;
+    }
 }
