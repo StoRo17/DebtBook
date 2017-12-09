265037a7568e260fa7bb1eb3cec77eaa5c31c938
Maksim Potashev <storo1017@gmail.com>
2017-12-09 09:09:41 +0000

    Create test class

---
+++ tests/Feature/Controllers/DebtsHistoryControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Feature\Controllers;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class DebtsHistoryControllerTest extends TestCase
+{
+    use RefreshDatabase;
+}
