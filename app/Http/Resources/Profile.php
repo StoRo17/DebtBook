0221c195a482c07b67b86d7b369e8f62c251fc9e
Maksim Potashev <storo1017@gmail.com>
2017-12-09 11:16:19 +0000

    Create Profile resource

---
+++ app/Http/Resources/Profile.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class Profile extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'avatar' => $this->avatar
+        ];
+    }
+}
