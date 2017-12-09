25b94874e422526febd27b8a68f50b3545f37c45
Maksim Potashev <storo1017@gmail.com>
2017-12-09 09:28:20 +0000

    Add creation of debt history method

--- app/Http/Controllers/DebtsHistoryController.php
+++ app/Http/Controllers/DebtsHistoryController.php
@@ -3,21 +3,38 @@
 namespace App\Http\Controllers;
 
 use App\DebtsHistory;
+use App\Http\Requests\DebtsHistoryCreationRequest;
+use App\Http\Resources\Debt;
 use App\Http\Resources\DebtsHistory as DebtsHistoryResource;
 
 class DebtsHistoryController extends Controller
 {
+    private $debt;
+
     private $debtsHistory;
 
-    public function __construct(DebtsHistory $debtsHistory)
+    public function __construct(Debt $debt, DebtsHistory $debtsHistory)
     {
+        $this->debt = $debt;
         $this->debtsHistory = $debtsHistory;
     }
 
     public function index($userId, $debtId)
     {
-        $debtHistory = $this->debtsHistory->where('debt_id', $debtId);
+        $debtHistory = $this->debtsHistory->where('debt_id', $debtId)->get();
 
         return DebtsHistoryResource::collection($debtHistory);
+    }
+
+    public function create(DebtsHistoryCreationRequest $request, $userId, $debtId)
+    {
+        $totalAmount = $request->type == 'give' ? $request->amount : -$request->amount;
+
+        $debt = $this->debt->where('debt_id', $debtId)->get();
+        $debt->total_amount += $totalAmount;
+        $debtHistory = $debt->history()->create($request->all());
+        $debt->save();
+
+        return new DebtsHistoryResource($debtHistory);
     }
 }
