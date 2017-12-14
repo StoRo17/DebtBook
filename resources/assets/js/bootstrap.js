ab7841207aab960ef53b2040f89b9303b60b8d1d
Maksim Potashev <storo1017@gmail.com>
2017-12-14 16:19:49 +0300

    Import and use vuex

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Vuex from 'vuex';
 import axios from 'axios';
 import Lang from  './localization/lang';
 
 
 window.Vue = Vue;
 Vue.use(VueRouter);
+Vue.use(Vuex);
 
 window.axios = axios;
 window.Lang = Lang;
