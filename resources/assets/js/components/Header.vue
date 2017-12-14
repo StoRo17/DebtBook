266f5d4302fc86b011b925f83f9ae8955b67ab21
Maksim Potashev <storo1017@gmail.com>
2017-12-14 17:04:32 +0300

    Change Header and remove it to components folder

---
+++ resources/assets/js/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <nav class="green">
+            <div class="nav-wrapper">
+                <router-link to="/" class="brand-logo" exact>DebtBook</router-link>
+                <div id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><router-link :to="{name: 'login'}">@{{ 'auth.sign_in' | trans }}</router-link></li>
+                    <li><router-link :to="{name: 'register'}">@{{ 'auth.sign_up' | trans }}</router-link></li>
+                </div>
+                <div id="slide-out" class="side-nav">
+                    <li><router-link :to="{name: 'register'}">@{{ 'auth.sign_up' | trans }}</router-link></li>
+                    <li><router-link :to="{name: 'login'}">@{{ 'auth.sign_in' | trans }}</router-link></li>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
