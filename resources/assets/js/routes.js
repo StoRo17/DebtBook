d93d1747557be21b1c238efa09a79830734a0352
Maksim Potashev <storo1017@gmail.com>
2017-12-14 17:40:51 +0300

    Update routes

--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -1,8 +1,17 @@
 import VueRouter from 'vue-router';
+import StartComponent from "./components/Start.vue";
 import RegisterForm from './components/forms/Register.vue';
 import LoginForm from './components/forms/Login.vue';
+import VerificationComponent from './components/Verification.vue';
+import EmailConfirmedComponent from './components/EmailConfirmed.vue';
+import NotFoundComponent from './components/NotFound.vue';
 
 let routes = [
+    {
+        path: '/',
+        name: 'start',
+        component: StartComponent
+    },
     {
         path: '/sign-up',
         name: 'register',
@@ -12,6 +21,20 @@ let routes = [
         path: '/sign-in',
         name: 'login',
         component: LoginForm
+    },
+    {
+        path: '/verification',
+        name: 'verification',
+        component: VerificationComponent
+    },
+    {
+        path: '/email-confirmed',
+        name: 'emailConfirmed',
+        component: EmailConfirmedComponent
+    },
+    {
+        path: '*',
+        component: NotFoundComponent
     }
 ];
 
