6706d1ad561906cd0026e53d0fb7f87fdf62ae54
Maksim Potashev <storo1017@gmail.com>
2017-12-14 17:27:24 +0300

    Put Header in App component

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -1,9 +1,15 @@
 <template>
-    
+    <div id="app">
+        <header-nav></header-nav>
+    </div>
 </template>
 
 <script>
+import HeaderNav from './components/Header.vue';
+
 export default {
-  
+    components: {
+        HeaderNav
+    }
 }
 </script>
