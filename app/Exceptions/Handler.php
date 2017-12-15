a4868d0e3512b9b0cd35c9db9a0b109fe125921f
Maksim Potashev <storo1017@gmail.com>
2017-12-15 15:34:43 +0300

    Add exception handlers

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -48,6 +50,18 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof AuthenticationException) {
+            return response()->json([
+                'error' => 'Not authenticated.'
+            ], 401);
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json([
+                'error' => 'Resource not found.'
+            ], 404);
+        }
+
         return parent::render($request, $exception);
     }
 }
