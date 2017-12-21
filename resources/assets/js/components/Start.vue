befbae602073f1eec69eaf1d85fdd8ac32bcc518
Maksim Potashev <storo1017@gmail.com>
2017-12-21 17:50:04 +0300

    Change start component

--- resources/assets/js/components/Start.vue
+++ resources/assets/js/components/Start.vue
@@ -1,12 +1,8 @@
 <template>
-    <div class="parallax-container">
-        <div class="section">
-            <div class="row container">
-                <h2>Debt Book</h2>
-                <router-link class="waves-light btn-large red"
-                             :to="{name: 'register'}">{{ 'auth.sign_up' | trans }}
-                </router-link>
-            </div>
-        </div>
+    <div>
+        <h2>Debt Book</h2>
+        <router-link class="waves-light btn-large red"
+                        :to="{name: 'register'}">{{ 'auth.sign_up' | trans }}
+        </router-link>
     </div>
 </template>
