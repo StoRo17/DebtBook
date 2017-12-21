b877bc96745772d9f23719b48dda131202a1799c
Maksim Potashev <storo1017@gmail.com>
2017-12-21 23:04:38 +0300

    Add checker before each request

--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -6,6 +6,19 @@ import App from './App.vue';
 
 Vue.component('app', App); 
 
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (!store.getters.isLoggedIn) {
+            next({name: 'login'});
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+})
+
 const vm = new Vue({
     el: '#root',
     router,
