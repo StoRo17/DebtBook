2e4afabe99ff9e4b31c1d3a688641bb37c2398b8
Maksim Potashev <storo1017@gmail.com>
2017-12-21 19:46:08 +0300

    Update header component

--- resources/assets/js/components/Header.vue
+++ resources/assets/js/components/Header.vue
@@ -4,13 +4,24 @@
             <div class="nav-wrapper">
                 <router-link to="/" class="brand-logo center" exact>DebtBook</router-link>
                 <a href="" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
-                <ul class="right hide-on-med-and-down">
-                    <li><router-link :to="{name: 'login'}">{{ 'auth.sign_in' | trans }}</router-link></li>
-                    <li><router-link :to="{name: 'register'}">{{ 'auth.sign_up' | trans }}</router-link></li>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <template v-if="isLoggedIn">
+                        <li><router-link :to="{name: 'profile'}">{{ fullName }}</router-link></li>
+                        <img :src="profile.avatar" class="circle">
+                    </template>
+                    <template v-else>
+                        <li><router-link :to="{name: 'login'}">{{ 'auth.sign_in' | trans }}</router-link></li>
+                        <li><router-link :to="{name: 'register'}">{{ 'auth.sign_up' | trans }}</router-link></li>
+                    </template>
                 </ul>
                 <ul id="slide-out" class="side-nav">
-                    <li><router-link :to="{name: 'register'}">{{ 'auth.sign_up' | trans }}</router-link></li>
-                    <li><router-link :to="{name: 'login'}">{{ 'auth.sign_in' | trans }}</router-link></li>
+                    <template v-if="isLoggedIn">
+                        <side-nav></side-nav>
+                    </template>
+                    <template v-else>       
+                        <li><router-link :to="{name: 'register'}">{{ 'auth.sign_up' | trans }}</router-link></li>
+                        <li><router-link :to="{name: 'login'}">{{ 'auth.sign_in' | trans }}</router-link></li>
+                    </template>
                 </ul>
             </div>
         </nav>
@@ -18,7 +29,25 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+import SideNav from './SideNav.vue';
+
 export default {
+    components: {
+        SideNav
+    },
+
+    computed: {
+        fullName() {
+            return this.profile.first_name + " " + this.profile.last_name;
+        },
+
+        ...mapGetters([
+            'isLoggedIn',
+            'profile'
+        ])
+    },
+
     mounted() {
             $( document ).ready(function(){
                 $(".button-collapse").sideNav({
