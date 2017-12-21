c5b9a8e0d46bfbcc7720869477632c7997853629
Maksim Potashev <storo1017@gmail.com>
2017-12-21 19:46:46 +0300

    Add getting user and profile in App component

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -8,11 +8,40 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+import api from './api/debtbook';
 import HeaderNav from './components/Header.vue';
 
 export default {
     components: {
         HeaderNav
+    },
+
+    computed: {
+        ...mapGetters([
+            'isLoggedIn',
+            'userId'
+        ])
+    },
+
+    created() {
+        if (this.isLoggedIn) {
+            api.getUser(this.userId)
+                .then(response => {
+                    this.$store.dispatch('setUser', response.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+
+            api.getProfile(this.userId)
+                .then(response => {
+                    this.$store.dispatch('setProfile', response.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        }
     }
 }
 </script>
