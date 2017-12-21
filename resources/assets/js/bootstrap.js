811818b29eb9cdfc9607c205f5fa595b470a6230
Maksim Potashev <storo1017@gmail.com>
2017-12-21 18:56:35 +0300

    Add headers to request

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -17,6 +17,10 @@ Vue.filter('trans', (...args) => {
 });
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+if (localStorage['access_token']) {
+    window.axios.defaults.headers.common['Accept'] = 'application/json';
+    window.axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage['access_token'];
+}
 
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
