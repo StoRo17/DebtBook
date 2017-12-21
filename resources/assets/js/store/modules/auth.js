3fcf790f3322f876fe1ab12dab5cb19e2b97405f
Maksim Potashev <storo1017@gmail.com>
2017-12-21 18:18:59 +0300

    Add user id in auth module

--- resources/assets/js/store/modules/auth.js
+++ resources/assets/js/store/modules/auth.js
@@ -2,6 +2,7 @@ import * as types from '../mutation-types';
 
 const state = {
     isLoggedIn: !!localStorage['access_token'],
+    userId: localStorage['user_id'],
     accessToken: localStorage['access_token'],
     expiresIn: localStorage['expires_in']
 }
@@ -22,26 +23,34 @@ const mutations = {
         state.expiresIn = data.expires_in;
     },
 
+    [types.SET_USER_ID](state, id) {
+        state.userId = id;
+    },
+
     [types.LOGIN](state) {
         state.isLoggedIn = true;
     },
 
     [types.LOGOUT](state) {
         state.isLoggedIn = false;
+        state.userId = null;
         state.accessToken = '';
         state.expiresIn = null;
     }
 }
 
 const actions = {
-    login({ commit }, tokenData) {
-        localStorage['access_token'] = tokenData.access_token;
-        localStorage['expires_in'] = tokenData.expires_in;
-        commit(types.SET_TOKEN, tokenData);
+    login({ commit }, data) {
+        localStorage['user_id'] = data.user_id;
+        localStorage['access_token'] = data.tokens.access_token;
+        localStorage['expires_in'] = data.tokens.expires_in;        
+        commit(types.SET_TOKEN, data.tokens);
+        commit(types.SET_USER_ID, data.user_id);
         commit(types.LOGIN);
     },
 
     logout({ commit }) {
+        delete localStorage['user_id'];
         delete localStorage['access_token'];
         delete localStorage['expires_in'];
         commit(types.LOGOUT);
--- resources/assets/js/store/mutation-types.js
+++ resources/assets/js/store/mutation-types.js
@@ -1,3 +1,4 @@
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
 export const SET_TOKEN = 'SET_TOKEN';
+export const SET_USER_ID = 'SET_USER_ID';
