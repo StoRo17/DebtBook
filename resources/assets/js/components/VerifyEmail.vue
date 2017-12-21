dff910f99c4a19dd62948e4974efc8b70a086100
Maksim Potashev <storo1017@gmail.com>
2017-12-21 12:27:15 +0300

    Add email token checking to verify email component

--- resources/assets/js/components/VerifyEmail.vue
+++ resources/assets/js/components/VerifyEmail.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <div class="row">
+        <div class="row" v-if="loaded">
             <h5 class="center-align">Your Email is successfully verified. Click button below to login</h5>
             <div class="center-align">
                 <router-link class="waves-light btn-large"
@@ -10,3 +10,22 @@
         </div>
     </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        loaded: false;
+    },
+    created() {
+        axios.get('/api/verify-email/' + this.$route.params.email_token)
+        .then(response => {
+            console.log(response.data);
+            this.loaded = true;
+        })
+        .catch(error => {
+        });
+    }
+}
+</script>
