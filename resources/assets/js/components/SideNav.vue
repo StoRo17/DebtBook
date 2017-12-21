7ed100725ceca6040871bc34bb4a169ba62771fa
Maksim Potashev <storo1017@gmail.com>
2017-12-21 19:46:23 +0300

    Update side nav component

--- resources/assets/js/components/SideNav.vue
+++ resources/assets/js/components/SideNav.vue
@@ -3,19 +3,30 @@
         <li>
             <div class="user-view">
                 <div class="background green"></div>
-                <router-link to="/" exact><img class="circle responsive-img" src="storage/avatars/no_image.jpg"></router-link>
-                <router-link to="/" exact><span class="white-text name">John Doe</span></router-link>
-                <router-link to="/" exact><span class="white-text email">johndoe@gmail.com</span></router-link>
+                <router-link to="/" exact><img class="circle responsive-img" :src="profile.avatar"></router-link>
+                <router-link to="/" exact><span class="white-text name">{{ fullName }}</span></router-link>
+                <router-link to="/" exact><span class="white-text email">{{ user.email }}</span></router-link>
             </div>
         </li>
-        <li><router-link to="/debts-history" class="waves-effect"><i class="material-icons">book</i>История долгов</router-link></li>
+        <li><router-link :to="{name: 'profile'}" class="waves-effect"><i class="material-icons">assignment_ind</i>Профиль</router-link></li>
         <li><div class="divider"></div></li>
-        <li><router-link to="/logout" caption="auth.logout"></router-link></li>
+        <li><router-link to="/logout">{{ 'auth.logout' | trans }}</router-link></li>
     </div>
 </template>
 
 <script>
-    export default {
+import { mapGetters } from 'vuex';
 
+export default {
+    computed: {
+        fullName() {
+            return this.profile.first_name + " " + this.profile.last_name;
+        },
+
+        ...mapGetters([
+            'user',
+            'profile'
+        ])
     }
+}
 </script>
