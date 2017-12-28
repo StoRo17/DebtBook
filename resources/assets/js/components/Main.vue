a9d403b020eff9ffd3df00247d4e2446b3353b13
Maksim Potashev <storo1017@gmail.com>
2017-12-29 00:34:46 +0300

    Small changes

--- resources/assets/js/components/Main.vue
+++ resources/assets/js/components/Main.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="container">
-        <template v-if="isLoggedIn">
+    <div>
+        <template v-if="this.$store.getters.isLoggedIn">
             <div class="row hide-on-med-and-down">
             <div id="add-debt-btn" class="center">
-                <router-link  to="/new-debt" class="waves-effect waves-light btn-large green lighten-1">
+                <router-link  :to="{name: 'newDebt'}" class="waves-effect waves-light btn-large green lighten-1">
                     <i class="material-icons right">add_box</i>{{ 'main.new_debt' | trans }}
                 </router-link>
             </div>
@@ -15,15 +15,8 @@
             <div class="row">
                 <debts-collection></debts-collection>
             </div>
-            <div class="row">
-                <h4>{{ 'main.owes' | trans }}</h4>
-                <div class="divider"></div>
-            </div>
-            <div class="row">
-                <debts-collection></debts-collection>
-            </div>
             <div class="fixed-action-btn hide-on-large-only">
-                <router-link to="/new-debt" class="btn-floating btn-large red waves-effect waves-light">
+                <router-link :to="{name: 'newDebt'}" class="btn-floating btn-large red waves-effect waves-light">
                     <i class="large material-icons">mode_edit</i>
                 </router-link>
             </div>
@@ -35,19 +28,13 @@
 </template>
 
 <script>
-    import Start from './Start.vue'
-    import DebtsCollection from './DebtsCollection.vue';
+import Start from './Start.vue'
+import DebtsCollection from './DebtsCollection.vue';
 
-    export default {
-        components: {
-            Start,
-            DebtsCollection
-        },
-
-        computed: {
-            isLoggedIn() {
-                return this.$store.getters.isLoggedIn;
-            }
-        }
+export default {
+    components: {
+        Start,
+        DebtsCollection
     }
+}
 </script>
