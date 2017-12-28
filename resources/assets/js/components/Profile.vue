9697f97ebda3ecdec028dfb24423263b259efcdf
Maksim Potashev <storo1017@gmail.com>
2017-12-28 20:26:51 +0300

    Add container class to root div

--- resources/assets/js/components/Profile.vue
+++ resources/assets/js/components/Profile.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="container">
         <div class="row">
             <div class="col s12 center-align hide-on-large-only">
                 <a href="/" class="waves-light btn" style="margin-top: 10px;">Назад</a>
