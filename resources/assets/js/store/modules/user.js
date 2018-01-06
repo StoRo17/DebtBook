9be49ada2016c053f77d0cee45413374d9b50572
Maksim Potashev <storo1017@gmail.com>
2018-01-06 16:03:58 +0300

    Add an action that loads the user

--- resources/assets/js/store/modules/user.js
+++ resources/assets/js/store/modules/user.js
@@ -1,3 +1,4 @@
+import api from '../../api/debtbook';
 import * as types from '../mutation-types';
 
 
@@ -18,8 +19,18 @@ const mutations = {
 }
 
 const actions = {
-    setUser({ commit }, user) {
-        commit(types.SET_USER, user);
+    loadUser({ commit }, userId) {
+        commit(types.LOADING_START);
+        api.getUser(userId)
+            .then(response => {
+                commit(types.SET_USER, response.data);
+                commit(types.LOADING_STOP);
+            })
+            .catch(error => {
+                console.log(error);
+                commit(types.ERROR);
+                commit(types.LOADING_STOP);
+            });
     }
 }
 
