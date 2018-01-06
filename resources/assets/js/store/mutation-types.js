94756059c1d73ab887cea91808a74058d990390c
Maksim Potashev <storo1017@gmail.com>
2018-01-06 16:41:58 +0300

    Add new mutation and write comments

--- resources/assets/js/store/mutation-types.js
+++ resources/assets/js/store/mutation-types.js
@@ -1,15 +1,23 @@
+//index mutations
 export const LOADING_START = 'LOADING_START';
 export const LOADING_STOP = 'LOADING_STOP';
 export const ERROR = 'ERROR';
 
+//auth mutations 
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
 export const SET_TOKEN = 'SET_TOKEN';
 export const SET_USER_ID = 'SET_USER_ID';
 
+//user mutations
 export const SET_USER = 'SET_USER';
 
+//profile mutations
 export const SET_PROFILE = 'SET_PROFILE';
 
+//debts mutations
 export const SET_DEBTS = 'SET_DEBTS';
 export const ADD_DEBT = 'ADD_DEBT';
+
+//currencies mutations
+export const SET_CURRENCIES = 'SET_CURRENCIES';
