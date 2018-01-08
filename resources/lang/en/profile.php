35bf2f96049874164a3b3a879eafc0a27b938fd4
Maksim Potashev <storo1017@gmail.com>
2018-01-09 02:22:56 +0300

    Create profile localization files

---
+++ resources/lang/en/profile.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'first_name' => 'First Name',
+    'last_name' => 'Last Name',
+    'profile' => 'Profile',
+    'change_data' => 'Change Data',
+    'edit_profile' => 'Edit Profile',
+];
