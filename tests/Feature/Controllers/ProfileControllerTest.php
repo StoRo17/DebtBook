b05844fa1d536792113a55f39f994ab9caeabb90
Maksim Potashev <storo1017@gmail.com>
2018-01-09 01:05:06 +0300

    Remove not needed tests

--- tests/Feature/Controllers/ProfileControllerTest.php
+++ tests/Feature/Controllers/ProfileControllerTest.php
@@ -44,7 +44,6 @@ class ProfileControllerTest extends TestCase
         $this->assertDatabaseHas('profiles', [
             'first_name' => $firstName,
             'last_name' => $lastName,
-            'avatar' => '/storage/avatars/no_image.jpg'
         ]);
         $response->assertJson([
             'success' => true,
@@ -54,45 +53,6 @@ class ProfileControllerTest extends TestCase
         $responseArray = $response->json();
         $this->assertEquals($firstName, $responseArray['data']['first_name']);
         $this->assertEquals($lastName, $responseArray['data']['last_name']);
-    }
-
-    public function testAvatarUpload()
-    {
-        Storage::fake('public');
-        $firstName = $this->user->profile->first_name;
-        $lastName = $this->user->profile->last_name;
-
-        $response = $this->putJson(route('updateProfile', $this->user->id),[
-            'first_name' => $firstName,
-            'last_name' => $lastName,
-            'avatar' => UploadedFile::fake()->image('avatar.jpg')
-        ]);
-
-        $this->assertDatabaseHas('profiles', [
-            'first_name' => $firstName,
-            'avatar' => "/storage/avatars/{$this->user->id}_avatar.jpg"
-        ]);
-
-        Storage::disk('public')->assertExists("avatars/{$this->user->id}_avatar.jpg");
-    }
-
-    public function testNotImageFormatFileDoesNotUpload()
-    {
-        Storage::fake('public');
-        $firstName = $this->user->profile->first_name;
-        $lastName = $this->user->profile->last_name;
-
-        $response = $this->putJson(route('updateProfile', $this->user->id),[
-            'first_name' => $firstName,
-            'last_name' => $lastName,
-            'avatar' => UploadedFile::fake()->create('avatar.txt')
-        ]);
-
-        $this->errorMessage['errors'] = [
-            'avatar' => [trans('validation.image', ['attribute' => 'avatar'])]
-        ];
-
-        $response->assertExactJson($this->errorMessage);
     }
 
     public function testReceiveErrorsWhenSendInvalidData()
