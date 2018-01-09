e1167eabda16dc7d06824043f85fbcde9c1e43aa
Maksim Potashev <storo1017@gmail.com>
2018-01-09 03:08:32 +0300

    Add localization for debts captions

---
+++ resources/lang/en/debts.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'add_debt' => 'Add a New Debt',
+    'name' => 'Name',
+    'amount' => 'Amount',
+    'currency' => 'Currency',
+    'give' => 'Give',
+    'take' => 'Take',
+    'comment' => 'Comment',
+    'delete_all' => 'Delete All',
+    'debt_history' => 'Debt History',
+    'edit_debt_history_element' => 'Edit the debt history element',
+];
---
+++ resources/lang/ru/debts.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'add_debt' => 'Add a New Debt',
+    'name' => 'Имя',
+    'amount' => 'Количество',
+    'currency' => 'Валюта',
+    'give' => 'Дать',
+    'take' => 'Взять',
+    'comment' => 'Комментарий',
+    'delete_all' => 'Удалить всё',
+    'debt_history' => 'История долга',
+    'edit_debt_history_element' => 'Удалить элемент истории долга',
+];
